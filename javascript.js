995b5d58bd581f1e35cdd1a4ba5a65a6b0a0e31d
Michael Anderson <Michael.anderson112890@gmail.com>
2023-04-04 16:59:50 -0700

    Added events for buttons and added a previous portion to the display

--- javascript.js
+++ javascript.js
@@ -1,3 +1,23 @@
+
+let pressed = '';
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (button.className === 'number'){
+            console.log(numberPressed(button.id));
+        }
+        else console.log(button.id);
+        pressed = button.id;
+        
+    })
+});
+
+
+
+
+
+
+
 function add(a,b){
     return a + b;
 }
@@ -24,5 +44,30 @@ function operate(a, b, operator){
             return multiply(a,b);
         default:
             return add(a,b);
+    }
+}
+
+function numberPressed(x){
+    switch (x) {
+        case ("one"):
+            return 1;
+        case ('two'):
+            return 2;
+        case ('three'):
+            return 3;
+        case ('four'):
+            return 4;
+        case ('five'):
+            return 5;
+        case ('six'):
+            return 6;
+        case ('seven'):
+            return 7;
+        case ('eight'):
+            return 8;
+        case ('nine'):
+            return 9;
+        default: 
+            return 0;
     }
 }
